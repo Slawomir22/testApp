b4411b16dc73f445f769e0f56e157dbc1e63a8cc
Slawek Majewski <majk71@10g.pl>
2023-10-24 19:29:10 +0200

    navigation component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MenubarModule } from 'primeng/menubar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavigationComponent
+	],
+	imports: [
+		BrowserModule,
+		MenubarModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+	selector: 'app-navigation',
+	templateUrl: './navigation.component.html',
+	styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+	items!: MenuItem[];
+
+	ngOnInit() {
+		this.items = [
+			{
+				label: 'Home',
+				icon: 'pi pi-fw pi-home',
+				routerLink: "/home"
+			},
+			{
+				label: 'Posts',
+				icon: 'pi pi-fw pi-list',
+				routerLink: "/posts"
+			},
+
+		];
+	}
 
 }
