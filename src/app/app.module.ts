525ef10fdb6ed9287605a7406147b054ebecc6a2
Slawek Majewski <majk71@10g.pl>
2023-10-24 20:01:05 +0200

    basic navigation  created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { PostsModule } from './posts/posts.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavigationComponent,
-  HomeComponent
+		HomeComponent
 	],
 	imports: [
 		BrowserModule,
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PostsComponent } from './posts.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 
 @NgModule({
-  declarations: [
-    PostsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+	declarations: [
+		PostsComponent
+	],
+	imports: [
+		CommonModule
+	]
 })
 export class PostsModule { }
